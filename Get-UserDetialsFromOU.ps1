9602dd6f0ead5619ea8d4c3303df895f2d39abcb
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2021-12-17 14:07:44 -0500

    Update Get-UserDetialsFromOU.ps1

--- Get-UserDetialsFromOU.ps1
+++ Get-UserDetialsFromOU.ps1
@@ -1,6 +1,5 @@
 $TimeStamp = Get-Date -Format "yyyyMMddTHHmmssffff"
 $CSVOutputPath = "$PSScriptRoot\Output\Get-UserDetialsFromOU-$TimeStamp.csv"
-$Properties = "Name", "EmailAddress"
 
 $OU = Read-Host "Enter the OU path"
 
@@ -9,4 +8,4 @@ Get-ADUser -Filter "Enabled -eq '$true'" -Properties * -SearchBase $OU `
     | Sort-Object Country, Office, Department, Title, Name `
     | Export-Csv -Path $CSVOutputPath -NoTypeInformation
 
-ii $CSVOutputPath
+Invoke-Item $CSVOutputPath
